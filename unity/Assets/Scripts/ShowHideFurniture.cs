725180c32a55ce8aba7c686facf192bc8a72508b
AVELL_TVG\diego <diegomazala@gmail.com>
2017-08-27 22:59:50 -0300

    Troca de sofás

---
+++ unity/Assets/Scripts/ShowHideFurniture.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideFurniture : MonoBehaviour
+{
+    public Animator animator;
+    public bool show = true;
+
+    private void Start()
+    {
+        if(animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+
+    public void Show ()
+    {
+        animator.SetTrigger("Show");
+        show = true;
+    }
+
+    public void Hide()
+    {
+        animator.SetTrigger("Hide");
+        show = false;
+    }
+
+    public void ShowHide(UnityEngine.UI.Toggle toggle)
+    {
+        show = toggle.isOn;
+        if (show)
+            animator.SetTrigger("Show");
+        else
+            animator.SetTrigger("Hide");
+
+    }
+
+    public void ShowHide()
+    {
+        show = !show;
+        if (show)
+            animator.SetTrigger("Show");
+        else
+            animator.SetTrigger("Hide");
+
+    }
+}
