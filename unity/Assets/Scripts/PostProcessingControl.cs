ee53dafcf570077f07433a34f49f130ad9950ed6
POPEYE\diego <diegomazala@gmail.com>
2017-09-11 21:21:11 -0300

    Added new animations for cameras

---
+++ unity/Assets/Scripts/PostProcessingControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessingControl : MonoBehaviour
+{
+    public UnityEngine.PostProcessing.PostProcessingBehaviour postProcessing;
+
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            postProcessing.enabled = !postProcessing.enabled;
+
+    }
+}
